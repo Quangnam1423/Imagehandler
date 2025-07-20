bb7948985e5b4a5cc7a67e82055d6fdcda2d5103
Quangnam1423 <suongthu2003@gmail.com>
2025-07-20 21:21:32 +0700

    inplement min pooling

--- Core/Source/Core.cpp
+++ Core/Source/Core.cpp
@@ -5,23 +5,54 @@
 
 #include "Manager/LoaderManager.h"
 
-#define _RESIZE_RATIO 16
+#define _RESIZE_RATIO 32
 
 cv::Mat resizeImageWithMinPooling(const cv::Mat& input, int resizeRatio)
 {
+	int newRows = input.rows / resizeRatio;
+	int newCols = input.cols / resizeRatio;
+	cv::Mat output(newRows, newCols, input.type());
 
+	for (int i = 0; i < newRows; i++)
+	{
+		for (int j = 0; j < newCols ; j++)
+		{
+			uchar minVal = 255;
+			for (int ki = 0; ki < resizeRatio; ++ki) {
+				for (int kj = 0; kj < resizeRatio; ++kj) {
+					int y = i * resizeRatio + ki;
+					int x = j * resizeRatio + kj;
+					uchar val = input.at<uchar>(y, x);
+					if (val < minVal) minVal = val;
+				}
+			}
+			output.at<uchar>(i, j) = minVal;
+		}
+	}
 
-	return cv::Mat();
+	return output;
 }
 
 int main() {
-	cv::Mat image = cv::imread("path/to/image.jpg", cv::IMREAD_COLOR);
-
+	cv::Mat image = cv::imread("D:/ImageHandler/Core/Resources/Images/rawimage.bmp", cv::IMREAD_GRAYSCALE);
 	if (image.empty()) {
 		std::cerr << "Error: Could not open or find the image!" << std::endl;
 		return -1;
 	}
-	
 
-	cv::Mat resizedImage = resizeImageWithMinPooling(image, _RESIZE_RATIO);
+	cv::flip(image, image, 0);
+	
+	auto start = std::chrono::high_resolution_clock::now();
+	cv::Mat resizedImage = resizeImageWithMinPooling(image, (int)_RESIZE_RATIO);
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli> duration = end - start;
+	std::cout << "Resizing took " << duration.count() << " ms" << std::endl;
+	cv::flip(image, image, 0);
+	cv::imwrite("D:/ImageHandler/Core/Resources/Images/gray_raw_image.bmp", image);
+
+	cv::flip(resizedImage, resizedImage, 0);
+	cv::imwrite("D:/ImageHandler/Core/Resources/Images/resizedimage.bmp", resizedImage);
+
+	cv::waitKey(0);
+	return 0;
 }
